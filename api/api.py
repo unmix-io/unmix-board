81f0d1eb2c7582f2860edc2b9232e7b25b576620
David Flury <david@flury.email>
2019-05-07 21:47:48 +0200

    Add initial stuff

---
+++ api/api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+"""
+unmix.io board RESTful API to receive information adn overview over unmix-net training runs.
+"""
+
+
+__author__ = 'David Flury, Andreas Kaufmann, Raphael Müller'
+__email__ = "info@unmix.io"
+
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+import os
+
+from controllers.dummy import DummyController
+
+
+def register_controllers(api):
+    api.add_resource(DummyController, '/dummy/<string:name>')
+
+def app():
+    app = Flask(__name__)
+    api = Api(app)
+    register_controllers(api)
+    CORS(app, resources={"*": {"origins": "*"}})
+    app.run('0.0.0.0', port=os.environ['UNMIX_BOARD_API_PORT'])
+
+
+if __name__ == "__main__":
+    app()
